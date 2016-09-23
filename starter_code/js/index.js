6bd988678bb82d5397873d00f97895502132a7e2
Georgie Brown <georginaabrown@gmail.com>
2016-09-23 16:08:07 +1000

    options added

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -1,1 +1,11 @@
-alert('hello');
+$(document).ready(start);
+//create an array with city names
+var cities = ['NYC', 'SF', 'LA', 'ATX', 'SYD'];
+
+// for(var i=0, i < cities.length; i++) {}
+
+
+function start(){
+  $('#city-type').change(doSomething)
+
+}
